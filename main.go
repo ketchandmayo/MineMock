fcd6f2df93824ac10caa18807f19a331c108f85c
ketchandmayo <83454515+ketchandmayo@users.noreply.github.com>
2026-02-14 17:46:59 +0500

    Add persistent logging and login metadata

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"os"
 
 	"MineMock/internal/config"
@@ -9,6 +11,16 @@ import (
 )
 
 func main() {
+	logFile, err := os.OpenFile("server.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
+	if err != nil {
+		fmt.Println("Failed to open server.log:", err)
+		os.Exit(1)
+	}
+	defer logFile.Close()
+
+	log.SetOutput(io.MultiWriter(os.Stdout, logFile))
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+
 	cfg := config.FromEnv()
 	addr := cfg.Address()
 
@@ -20,8 +32,21 @@ func main() {
 		OnlinePlayers: cfg.OnlinePlayers,
 	}
 
+	log.Printf(
+		"Server configuration loaded: ip=%s port=%s motd=%q version=%s protocol=%d max_players=%d online_players=%d error_delay=%s force_connection_lost_title=%t",
+		cfg.IP,
+		cfg.Port,
+		cfg.MOTD,
+		cfg.VersionName,
+		cfg.Protocol,
+		cfg.MaxPlayers,
+		cfg.OnlinePlayers,
+		cfg.ErrorDelay,
+		cfg.ForceConnectionLostTitle,
+	)
+
 	if err := server.Run(addr, cfg.ErrorMessage, cfg.ErrorDelay, cfg.ForceConnectionLostTitle, statusCfg); err != nil {
-		fmt.Println("Server error:", err)
+		log.Println("Server error:", err)
 		os.Exit(1)
 	}
 }
